c865d8906e1f7aa8304cdc69355f71f62a78d5db
toco4time <80077176+toco4time@users.noreply.github.com>
2022-03-08 18:31:04 -0600

    Set things up to begin work on the database system for stocks]

--- app/FormatStockData.py
+++ app/FormatStockData.py
@@ -2,15 +2,13 @@
 #import pandas as pd
 import yfinance as yf
 
-def FormatStockDataToNum(Token,Depth):
 
-    #Data = list()
+def FormatStockDataToNum(Token, Depth):
 
-    RawData = yf.Ticker(Token).history(period = str(Depth) + 'mo')
+    RawData = yf.Ticker(Token).history(period=str(Depth) + 'mo')
 
     PDat = RawData.get("Close")
 
     Data = PDat.to_numpy()
-    
-    return Data
 
+    return Data
---
+++ app/data/AddFile.py
@@ -0,0 +1,19 @@
+
+def CheckForRecursion(ticker):
+    pass
+
+
+def AddFile(ticker):
+    pass
+
+
+def AddData(FileName):
+    pass
+
+
+def CheckDataBase(ticker, depth):
+    pass
+
+
+if __name__ == '__main__':
+    CheckDataBase('AAPL')
