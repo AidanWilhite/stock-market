709744074dab03762da3aef81ae57a8c92038183
toco4time <80077176+toco4time@users.noreply.github.com>
2022-03-09 06:53:46 -0600

    Finished hacking those two together

--- app/data/dataBase.py
+++ app/data/dataBase.py
@@ -24,19 +24,15 @@ def RetrieveData(file, depth):
         # look through
 
     for line in lines:
-        # TODO look through data until we find :# and log the number we find inside
         if line[0] == ":" and int(line[1]) == depth:
-            print(SelectedData)
-            break
+            return SelectedData
         else:
             if line[0] != ":" and line[0] != "=":
-                print(line)
-                SelectedData.append(line)
+                SelectedData.append(float(line))
 
 
 def AddFile(ticker):
     with open(f'app/data/database/{ticker}.txt', 'w') as f:
-        # TODO get yahoo finance to write some data to this file and we are good to go!
         Data = (yfinance.Ticker("AAPL").history(
             period='32mo')).get("Close").to_numpy()
 
@@ -57,19 +53,13 @@ def AddFile(ticker):
                 l = l+1
 
 
-def AddData(FileName):
-    pass
-
-
 def CheckDataBase(ticker, depth):
 
     if os.path.exists(f'app/data/database/{ticker}.txt') is False:
         print("Making Data File")
         AddFile(ticker)
+        print("Getting Data")
+        return RetrieveData(f'app/data/database/{ticker}.txt', depth)
     else:
         print("File Found, retrieving data")
-        RetrieveData(f'app/data/database/{ticker}.txt', depth)
-
-
-if __name__ == '__main__':
-    CheckDataBase('AAPL', 4)
+        return RetrieveData(f'app/data/database/{ticker}.txt', depth)
