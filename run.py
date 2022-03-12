ed8d1a5c7363c4dc588bbaa32183abaa6aa19880
toco4time <80077176+toco4time@users.noreply.github.com>
2022-03-12 07:40:31 -0600

    Removing ussless imports

--- run.py
+++ run.py
@@ -1,45 +1,7 @@
 
-import cProfile
 import math
-from re import I
-from tkinter import Y, Canvas, Tk
 from app.Workers.LinRegWorkers import Work
-from app.calc import LinReg
-import app.calc.LinReg
 from app.data import getData
-
-
-def create_circle(x, y, r, canvasName, color):  # center coordinates, radius
-    x0 = x - r
-    y0 = y - r
-    x1 = x + r
-    y1 = y + r
-    return canvasName.create_oval(x0, y0, x1, y1, fill=color)
-
-
-def MakeGraph(Dat_x, Dat_y, XHigh, YHigh):
-
-    root = Tk()
-    root.geometry("800x800")
-
-    ScaleX = 600 / (XHigh)
-    ScaleY = 600 / (YHigh)
-
-    c = Canvas(root, height=800, width=800)
-
-    for i in range(len(Dat_y)):
-
-        create_circle((Dat_y[i] * ScaleY) + 100,
-                      (Dat_x[i] * ScaleX) - 200, 2, c, "black")
-
-        if (LinReg.GetInterRange() * 1.5) + LinReg.GetMean() > Dat_x[i] and (LinReg.GetInterRange() * -1.5) + LinReg.GetMean() < Dat_x[i]:
-
-            create_circle((Dat_y[i] * ScaleY) + 100,
-                          (LinReg.GetReg(i) * ScaleX) - 200, 2, c, "red")
-
-    c.pack()
-
-    root.mainloop()
 
 
 if __name__ == "__main__":
